6b715d59527caf3467bf4a9605c13837785b334a
GARCELLO <102846563+AmNotSLENDEROK14@users.noreply.github.com>
2022-10-30 12:38:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  vide
+}
+
+function draw () {
+
+}
+
+function take_snapshot(){
+  save('myFilterImage.png');
+}
